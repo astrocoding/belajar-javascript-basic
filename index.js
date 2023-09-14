f3a2eead9ec36b787199476ccbd5aa3dd3ffc8b4
astrocoding <reepalstudio01@gmail.com>
2023-09-15 00:09:34 +0700

    add .nmprc & modify index.js

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+import _ from 'lodash';
+const myArray = [1, 2, 3, 4];
+const sum = _.sum(myArray);
+
+console.log(sum)
+function multiply(num) {
+  total = num * num;
+}
+
+const hasil = multiply(3);
+
+console.log(hasil);
+
+
+let x;
+x = 7;
+x = 'JS is great'
+console.log(x);
+
+if((true || false) && (false || false)){
+  console.log("It true")
+} else {
+  console.log("it false")
+}
+
+const capital = {
+  "Jakarta": "Indonesia"
+}
+
+capital["New Delhi"] = "Indonesia";
+console.log(capital["Indonesia"]);
+
+function car({brand, maxSpeed, wheelCount}){
+  this.brand = brand;
+  this.maxSpeed = maxSpeed;
+  this.wheelCount = wheelCount;
+}
+
+const myCar = new car({brand:'toyota',maxSpeed: 200, wheelCount: 4});
+console.log(myCar);
+
+try {
+  const arr = [1, 2, 3, 4];
+  for (let i = 0; i <= 4; i++) {
+  console.log(arr[i]);
+  }
+  } catch(e) {
+  console.log("Out of bounds");
+  }
+
+
 console.log("Menyalakan mesin kopi");
 console.log("Menggiling biji kopi");
 console.log("Memanaskan air");
