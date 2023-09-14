6c437f954e257a91f18d0c4a2200e42b460135aa
astrocoding <reepalstudio01@gmail.com>
2023-09-14 12:11:21 +0700

    add 4_member_visibility.js to oop module

---
+++ oop/4_member_visibility.js
@@ -0,0 +1,38 @@
+class Car {
+  #chassisNumber = null; // enclosing class
+  constructor(brand, color, maxSpeed) {
+    this.brand = brand;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.#chassisNumber = this.#generateChassisNumber();
+  }
+
+  get chassisNumber() {
+    return this.#chassisNumber;
+  }
+
+  set chassisNumber(chassisNumber) {
+    console.log('You are not allowed to change the chassis number!');
+  }
+
+  // Method
+  drive() {
+    console.log(`${this.brand} ${this.color} is driving`);
+  }
+
+  reverse() {
+    console.log(`${this.brand} ${this.color} is reversing`);
+  }
+
+  turn(direction) {
+    console.log(`${this.brand} ${this.color} is turning ${direction}`);
+  }
+
+  #generateChassisNumber() {
+    return `${this.brand}-${Math.floor(Math.random() * 1000)}`;
+  }
+}
+
+const car = new Car('Mazda', 'Silver', 220);
+car.turn('Left');
+console.log(car);
