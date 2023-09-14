68131560f5eb25fd89655e29597e2128255a1f0e
astrocoding <reepalstudio01@gmail.com>
2023-09-14 15:01:43 +0700

    modified index.js, package.json and add state.js & state2.js

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+
+// const {coffeeStock, isCoffeeMachineReady} = require('./state');
+
+// console.log(coffeeStock);
+// console.log(isCoffeeMachineReady);
+
+// const makeCoffee = (type, miligrams) => {
+//   if (coffeeStock[type] >= miligrams) {
+//     console.log("Kopi berhasil dibuat!");
+//   } else {
+//     console.log("Biji kopi habis!");
+//   }
+// }
+// makeCoffee("robusta", 80);
+
+// import colorStock from './state2.js'; Untuk satu saja
+import { colorStock as stock, isColorReady } from './state2.js';
+
+const displayStock = stock => {
+  for (const type in stock) {
+    console.log(type);
+  }
+}
+
+displayStock(stock);
+console.log(stock);
+console.log(isColorReady);
---
+++ state2.js
@@ -0,0 +1,9 @@
+const colorStock = {
+  red: 1000,
+  black: 1500,
+  white: 2000
+};
+
+const isColorReady = true;
+
+export { colorStock, isColorReady };
