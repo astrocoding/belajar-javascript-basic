3c46e486e520b5f16aeed7eea6d1bd9e040cf7a5
astrocoding <reepalstudio01@gmail.com>
2023-09-18 10:15:52 +0700

    add index.html and konversi.js

---
+++ practices/konversi.js
@@ -0,0 +1,27 @@
+
+const a = [];
+const n = prompt('Masukan basis bilangan: ');
+const d = prompt('Masukan panjang digit: ');
+parseInt(n, d);
+
+// TODO: Input untuk nilai dari array (nilai digit)
+for(let i = 0; i < d;i++){
+  a[i] = prompt(`Masukan nilai digit ke-${i+1}`);
+}
+
+console.log(a);
+// TODO: Konversi tipe data untuk array
+let numberArr = [];
+for(let i = 0; i < a.length; i++) {
+  numberArr.push(parseInt(a[i]));
+}
+
+// TODO: Operasi perhitungan hasil konversi sistem bilangan
+console.log(numberArr);
+let hasil = 0;
+for(let i = 0; i < d; i++){
+  hasil = numberArr[i] + hasil * n;
+  console.log(hasil);
+}
+console.log(`Hasil konversi ke bilangan desimal: ${hasil}`);
+alert(`Hasil konversi ke bilangan desimal: ${hasil}`);
